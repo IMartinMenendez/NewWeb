ce5a75b182fe599bee34e9f94d022a5093a5eef3
Irene Martin <irene1martin2menendez@gmail.com>
2021-10-20 21:07:05 +0100

    initial version with all components and some css working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WrapperComponent } from './wrapper/wrapper.component';
+import { IntroComponent } from './intro/intro.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FeatureComponent } from './feature/feature.component';
+import { PostsComponent } from './posts/posts.component';
+import { PaginationComponent } from './pagination/pagination.component';
+import { FooterComponent } from './footer/footer.component';
+import { CopyrightComponent } from './copyright/copyright.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WrapperComponent,
+    IntroComponent,
+    HeaderComponent,
+    NavbarComponent,
+    FeatureComponent,
+    PostsComponent,
+    PaginationComponent,
+    FooterComponent,
+    CopyrightComponent
   ],
   imports: [
     BrowserModule
